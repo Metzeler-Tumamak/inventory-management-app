dd9534cb64e29c8975f9587109bae8fcc8646bbf
Metzeler Tumamak <208787873+Metzeler-Tumamak@users.noreply.github.com>
2026-06-10 14:50:12 +0800

    feat: Add product route and controller

---
+++ src/controllers/products.js
@@ -0,0 +1,45 @@
+const db = require("../db/queries");
+
+async function createProduct(req, res) {
+  let newProduct = null;
+  try {
+    newProduct = await db.createProduct(req.body);
+  } catch (err) {
+    throw new Error(
+      "Issue encountered when creating new product: " + err.message,
+    );
+  }
+
+  res.json(newProduct);
+}
+
+async function updateProduct(req, res) {
+  const { id, data } = req.body;
+  let updatedProduct = null;
+
+  try {
+    updateProduct = await db.putUpdateProduct(id, data);
+  } catch (err) {
+    throw new Error("Error encounterd when updating product: " + err.message);
+  }
+
+  res.json(updateProduct);
+}
+
+async function deleteProduct(req, res) {
+  const { id } = req.body;
+
+  try {
+    await db.deleteProductById(id);
+  } catch (err) {
+    throw new Error("Error encountered when deleting product: " + err.message);
+  }
+
+  res.status(200).end();
+}
+
+module.exports = {
+  createProduct,
+  updateProduct,
+  deleteProduct,
+};
