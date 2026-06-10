39715c29905ed6140afeb11f48ed8802555b0a50
Metzeler Tumamak <208787873+Metzeler-Tumamak@users.noreply.github.com>
2026-06-10 14:50:12 +0800

    feat: Add product route and controller

---
+++ src/routes/products.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const controller = require("../controllers/products");
+
+const router = Router();
+
+router
+  .route("/")
+  .post(controller.createProduct)
+  .put(controller.updateProduct)
+  .delete(controller.deleteProduct);
