e40291ddded38f1a582e14d59eee844b35c793bb
Metzeler Tumamak <208787873+Metzeler-Tumamak@users.noreply.github.com>
2026-06-16 14:48:47 +0800

    feat: Add dropdown for add menu

--- public/index.js
+++ public/index.js
@@ -1,3 +1,10 @@
 lucide.createIcons();
 
 console.log(products, categories);
+
+const addMenuBtn = document.querySelector(".add-menu");
+const addDropdown = document.querySelector(".add-dropdown");
+
+addMenuBtn.addEventListener("click", (event) => {
+  addDropdown.classList.toggle("hidden");
+});
