4454509b68c1ce94d8e7199b6d0a5a1ba5cbbcf7
Metzeler Tumamak <208787873+Metzeler-Tumamak@users.noreply.github.com>
2026-06-17 12:54:37 +0800

    style: Update loader

--- public/index.js
+++ public/index.js
@@ -2,6 +2,8 @@ lucide.createIcons();
 
 console.log(products, categories);
 
+const loader = document.querySelector(".loader-wrapper");
+
 const addMenuBtn = document.querySelector(".add-menu");
 const addDropdown = document.querySelector(".add-dropdown");
 
@@ -11,9 +13,24 @@ addMenuBtn.addEventListener("click", (event) => {
 
 const addProductBtn = document.querySelector(".add-product-btn");
 const addProductModal = document.querySelector(".add-product-modal");
+const addProductForm = document.querySelector(".add-product-form");
+const modalContent = document.querySelector(".modal-content");
 
 addProductBtn.addEventListener("click", (e) => {
   addProductModal.showModal();
   e.stopPropagation();
+  addProductForm.classList.toggle("hidden");
   addDropdown.classList.toggle("hidden");
 });
+
+addProductForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  for (const [key, value] of new FormData(e.target)) {
+    console.log(key, value);
+  }
+  modalContent.classList.toggle("hidden");
+  loader.classList.toggle("hidden");
+});
+
+const clickEvent = new Event("click");
+addProductBtn.dispatchEvent(clickEvent);
